e3973b8d686c6d242e94ce228a6c6e054b7ce49b
Brandon Van Dyck <bvandyck@shyftanalytics.com>
2019-01-18 19:47:36 -0500

    Setup basic new poll component

--- src/components/NewPoll.js
+++ src/components/NewPoll.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react';
 class NewPoll extends Component {
   render() {
     return (
-      <div>
-        NewPoll
+      <div className="new-poll">
+        <h1 className="new-poll-header"> Create New Question </h1>
+
+        <div className="new-poll-question flex-column">
+          <span> Complete the question:</span>
+          <h3> Would you rather ... </h3>
+          <input placeholder="Enter Option One text here"/>
+          <span> OR </span>
+          <input placeholder="Enter Option Two text here" />
+          <button className="submit-poll-btn"> Submit </button>
+        </div>
+        
       </div>
     )
   }

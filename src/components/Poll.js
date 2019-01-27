3abfd65ad398d40f933287baa99611d1989cb090
Brandon Wieber <brandon.m.wieber@gmail.com>
2019-01-27 11:17:12 -0500

    refactor: use poll component instead of hardcoded poll

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -2,19 +2,28 @@ import React, { Component } from 'react';
 
 class Poll extends Component {
   render() {
+    const { id, author, avatar, questionText } = this.props;
+    
     return (
-      <div className="poll flex-column">
+      <div key={id} className="poll flex-column">
         <div className="poll-header">
-           <h3 className="poll-header--text"> Name asks: </h3>
+           <h3 className="poll-header--text"> {author} asks: </h3>
         </div>
 
         <div className="poll-question-group flex-row">
-          <div className="poll-avatar"> 
-            <div className="avatar"> </div>
+          <div className="poll-avatar">
+            <div className="avatar">
+              <img
+                src={avatar}
+                alt="Avatar"
+                width="100"
+                height="100"
+              />
+            </div>
           </div>
           <div className="poll-question flex-column">
             <h3> Would you rather </h3>
-            <span> ...the question... </span>
+            <span> ...{questionText}...</span>
             <button className="view-poll-btn"> View Poll </button>
           </div>
         </div>
--- src/components/PollList.js
+++ src/components/PollList.js
@@ -13,31 +13,12 @@ class PollList extends Component {
 
         <div className="poll-group">
           {this.props.questions.map((question) => (
-            <div key={question.id} className="poll flex-column">
-              <div className="poll-header">
-                 <h3 className="poll-header--text"> {question.author} asks: </h3>
-              </div>
-
-              <div className="poll-question-group flex-row">
-                <div className="poll-avatar">
-                  <div className="avatar">
-                    <img
-                      src={this.props.users.find((user) => user.id === question.author).avatarURL}
-                      alt="Avatar"
-                      width="100"
-                      height="100"
-                    />
-                  </div>
-                </div>
-                <div className="poll-question flex-column">
-                  <h3> Would you rather </h3>
-                  <span> ...{question.optionOne.text}...</span>
-                  <button className="view-poll-btn"> View Poll </button>
-                </div>
-              </div>
-
-            </div>
-
+            <Poll
+              id={question.id}
+              author={question.author}
+              avatar={this.props.users.find((user) => user.id === question.author).avatarURL}
+              questionText={question.optionOne.text}
+              />
           ))}
         </div>
       </div>

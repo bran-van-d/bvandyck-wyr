04c00a7c4b55a8bcff91f9df39c48ea0ec528eed
Brandon Wieber <brandon.m.wieber@gmail.com>
2019-01-27 13:26:26 -0500

    feat: connect leaderboard to data and create score-card component

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,35 +1,51 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Scorecard from './Scorecard';
 
 class Leaderboard extends Component {
   render() {
+    const { users, questions } = this.props;
+
     return (
-      // TODO: This needs to be refactored to benefit from a base "card" component or something similar.
-      <div className="poll-list flex-column">
-        <div className="poll flex-column">
-          <div className="poll-header">
-            <h3 className="poll-header--text"> Name asks: </h3>
+      <div>
+        {users.map((user) => (
+          <div className="poll-list flex-column">
+            <div className="poll flex-column">
+              <div className="poll-header">
+                <h3 className="poll-header--text"> {user.name} asks: </h3>
+              </div>
+
+              <div className="poll-question-group flex-row">
+                <div className="poll-avatar">
+                  <div className="avatar">
+                    <img
+                      src={user.avatarURL}
+                      alt="Avatar"
+                      width="100"
+                      height="100"
+                      />
+                  </div>
+                </div>
+
+                <Scorecard
+                  user={user}
+                  questions={questions}
+                />
+              </div>
+            </div>
           </div>
+        ))}
 
-          <div className="poll-question-group flex-row">
-            <div className="poll-avatar">
-              <div className="avatar"> </div>
-            </div>
-
-            <div className="leader-board-question-type flex-column">
-              <h3> Name </h3>
-              <span> Answered Questions: 7 </span>
-              <span> Answered Questions: 3 </span>
-            </div>
-
-            <div className="leader-board-score">
-              <h3 className="score-header"> Score </h3>
-              <div className="leader-board-score__bubble"></div>
-            </div>
-          </div>
-        </div>
       </div>
     )
   }
 }
 
-export default Leaderboard
+function mapStateToProps({ questions, users }) {
+  return {
+    questions: Object.values(questions),
+    users: Object.values(users)
+  }
+}
+
+export default connect(mapStateToProps)(Leaderboard)
---
+++ src/components/Scorecard.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class Scorecard extends Component {
+  render() {
+    const { user, questions } = this.props;
+
+    const answeredQuestions = user.questions.length;
+    const createdQuestions = questions.filter((question) => (
+      user.id === question.author
+    )).length;
+
+    const score = answeredQuestions + createdQuestions;
+
+    return (
+      <div className="score-card flex-row">
+        <div className="leader-board-question-type flex-column">
+          <h3> Name </h3>
+          <span> Answered Questions: {answeredQuestions} </span>
+          <span> Created Questions: {createdQuestions} </span>
+        </div>
+
+        <div className="leader-board-score">
+          <h3 className="score-header"> Score </h3>
+          <div className="leader-board-score__bubble">
+          {score}
+          </div>
+        </div>
+      </div>
+
+    )
+  }
+}
+
+
+
+
+export default Scorecard

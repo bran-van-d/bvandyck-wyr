4125c2a037fd6e83485168cc54bee409e8a3dea7
Brandon Van Dyck <bvandyck@shyftanalytics.com>
2019-02-12 19:36:21 -0500

    feat: sort answered and unanswered questions on the homepage

--- src/components/PollList.js
+++ src/components/PollList.js
@@ -3,16 +3,54 @@ import Poll from './Poll';
 import { connect } from 'react-redux';
 
 class PollList extends Component {
+  state = {
+    activeTab: 'UNANSWERED'
+  }
+
+  changeTab(tabName) {
+    this.setState(() => ({
+      activeTab: tabName
+    }))
+  }
+
   render() {
+    const { questions } = this.props;
+    const currentUser = 'sarahedo';
+
+    const answers = [];
+    const noAnswer = [];
+
+    questions.forEach((question) => {
+      const votedOne = question.optionOne.votes.includes(currentUser);
+      const votedTwo = question.optionTwo.votes.includes(currentUser);
+
+      if(votedOne || votedTwo) {
+        answers.push(question);
+      }
+      else {
+        noAnswer.push(question);
+      }
+    });
+
     return (
       <div className="poll-list flex-column">
         <div className="question-header flex-row">
-          <div className="question-type question-type--active"> Unanswered Questions </div>
-          <div className="question-type"> Answered Questions </div>
+          <div 
+            onClick={() => this.changeTab('UNANSWERED')} 
+            className={this.state.activeTab === 'UNANSWERED' ? "question-type question-type--active" : "question-type"  }
+            > Unanswered Questions 
+            </div>
+
+          <div 
+            onClick={() => this.changeTab('ANSWERED')} 
+            className={this.state.activeTab === 'ANSWERED' ? "question-type question-type--active" : "question-type"}
+            > Answered Questions 
+          </div>
         </div>
 
         <div className="poll-group">
-          {this.props.questions.map((question) => (
+          {this.state.activeTab === 'ANSWERED'
+          ? answers.map((question) => (
             <Poll
               key={question.id}
               id={question.id}
@@ -20,7 +58,17 @@ class PollList extends Component {
               avatar={this.props.users.find((user) => user.id === question.author).avatarURL}
               questionText={question.optionOne.text}
               />
+          ))
+          : noAnswer.map((question) => (
+            <Poll
+              key={question.id}
+              id={question.id}
+              author={question.author}
+              avatar={this.props.users.find((user) => user.id === question.author).avatarURL}
+              questionText={question.optionOne.text}
+            />
           ))}
+          
         </div>
       </div>
     )
@@ -28,10 +76,11 @@ class PollList extends Component {
 }
 
 
-function mapStateToProps({ questions, users }) {
+function mapStateToProps({ questions, users, authedUser }) {
   return {
     questions: Object.values(questions),
-    users: Object.values(users)
+    users: Object.values(users),
+    authedUser
   }
 }
 
--- src/components/Scorecard.js
+++ src/components/Scorecard.js
@@ -38,7 +38,4 @@ class Scorecard extends Component {
   }
 }
 
-
-
-
 export default Scorecard
